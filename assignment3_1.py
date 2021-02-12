a604fc54819516323cc7d49ce865115cb1e51491
mingfang <57225674+mingfanghu81@users.noreply.github.com>
2021-02-11 23:38:03 -0500

    Add files via upload

---
+++ assignment3_1.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Assume that you have developed a new classifier that achieves a sensitivity of 0.75 and a
+specificity of 0.80. Create a confusion matrix for three test sets comprising a) 100 samples in each
+class, b) 100 positive samples and 1000 negative samples, c) 20 samples in each class.
+
+Plot three precision-recall curves (on a single plot) for a random classifier applied in
+each of the three cases. Here, a “random classifier” will classify each test point with
+50% likelihood of assigning the positive class and 50% likelihood of negative.
+iv) Add three points to the precision-recall plot from part iii), illustrating your classifier’s
+performance in each of the three cases.
+"""
+
+import numpy
+import matplotlib.pyplot as plt
+from sklearn.metrics import precision_recall_curve
+"for the case, 100 positive samples and 100 negative samples "
+true=[0]*100+[1]*100
+numpy.random.shuffle(true)
+estimate = numpy.random.random(200)
+precision,recall,threshold=precision_recall_curve(true,estimate)
+plt.plot(recall,precision)
+plt.xlim(0,1)
+plt.ylim(0,1)
+plt.xlabel("recall")
+plt.ylabel("precision")
+plt.plot(recall[0],precision[0],'r*')
+plt.plot(recall[194],precision[194],'r*')
+plt.plot(recall[100],precision[100],'r*')
+plt.show()
+
+"for the case, 100 positive samples and 1000 negative samples"
+trueb=[1]*100+[0]*1000
+numpy.random.shuffle(trueb)
+estimateb= numpy.random.random(1100)
+precisionb,recallb,thresholdb=precision_recall_curve(trueb,estimateb)
+plt.figure()
+plt.plot(recallb,precisionb)
+plt.xlim(0,1)
+plt.ylim(0,1)
+plt.xlabel("recall")
+plt.ylabel("precision")
+plt.plot(recallb[0],precisionb[0],'r*')
+plt.plot(recallb[1009],precisionb[1009],'r*')
+plt.plot(recallb[550],precisionb[550],'r*')
+plt.show()
+
+"for the case, 20 positive samples and 20 negative samples "
+truec=[0]*20+[1]*20
+numpy.random.shuffle(truec)
+estimatec= numpy.random.random(40)
+precisionc,recallc,thresholdc=precision_recall_curve(truec,estimatec)
+plt.figure()
+plt.plot(recallc,precisionc)
+plt.xlim(0,1)
+plt.ylim(0,1)
+plt.xlabel("recall")
+plt.ylabel("precision")
+plt.plot(recallc[0],precisionc[0],'r*')
+plt.plot(recallc[39],precisionc[39],'r*')
+plt.plot(recallc[20],precisionc[20],'r*')
+plt.show()
+
