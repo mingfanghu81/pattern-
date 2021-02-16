d7cf225a6e6dd91503a489bdeacdc6eb75c22b64
mingfang <57225674+mingfanghu81@users.noreply.github.com>
2021-02-16 18:05:29 -0500

    Update get_accuracy.py

--- get_accuracy.py
+++ get_accuracy.py
@@ -5,7 +5,7 @@ get accuracy
 """
 import sklearn as sk
 
-def get_accuracy(gnb,X,y):
+def get_accuracy(gnb, X, y):
     y_predict = gnb.predict(X)
     y_accuracy = sk.metrics.accuracy_score(y, y_predict)
     return y_accuracy
